bfc3f5de96049b941cf26e450cffcf8ba920ed2b
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-06-10 07:11:46 -0400

    Clear all warnings for Netlify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 import { select } from "d3";
-import * as d3 from "d3";
+// import * as d3 from "d3";
 
 console.log('hello global');
 
@@ -17,12 +17,12 @@ function App() {
     let g01aW = +g01aWidthScaling * vptW;
     let g01aHeightScaling = "0.4";
     let g01aH = +g01aHeightScaling * vptH;
-    let g01aM = 40;
+    // let g01aM = 40;
     console.log(g01aW,g01aH);
 
-    var parse = d3.utcParse( "%m/%d/%Y" );
-    var format = d3.utcFormat( "%m/%Y" );
-    var format_with_days = d3.utcFormat( "%m/%d/%Y" );
+    // var parse = d3.utcParse( "%m/%d/%Y" );
+    // var format = d3.utcFormat( "%m/%Y" );
+    // var format_with_days = d3.utcFormat( "%m/%d/%Y" );
 
     const svg = select(svgRef.current);
     svg
