3f0d8586eb99fef1291d63ca63a7fa8ffb392a35
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-06-26 14:32:26 -0400

    Am partways through information flow re-write pt02

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { Route, Switch } from "react-router-dom";
-import QualityGraph from "./components/QualityGraph/QualityGraph";
-import QuantityGraph from "./components/QuantityGraph/QuantityGraph";
-import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
-import CreateForm from "./components/CreateForm/CreateForm";
-import DataList from './components/DataList/DataList';
+import DataPrep from "./components/DataPrep/DataPrep";
+// import QualityGraph from "./components/QualityGraph/QualityGraph";
+// import QuantityGraph from "./components/QuantityGraph/QuantityGraph";
+// import Home from "./components/Home/Home";
+// import CreateForm from "./components/CreateForm/CreateForm";
+// import DataList from './components/DataList/DataList';
 
 const Api = 
 "https://glorify-the-supreme-god-67d35a.herokuapp.com";
@@ -15,23 +16,13 @@ const Api =
 
 function App() {
 
+  // eslint-disable-next-line
+  const [isFromApp, setIsFromApp] = useState(true);
+
+  // eslint-disable-next-line
+  const [dataSeries, setDataSeries] = useState(null)
   const [qualityData, setQualityData] = useState(null);
   const [quantityData, setQuantityData] = useState(null);
-  const [dataSeries, setDataSeries] = useState(null)
-  const [data, setData] = useState(null)
-  const [forcedRedraw, setForcedRedraw] = useState(0);
-
-  function onCreate() {
-    setForcedRedraw((ps) => (ps+1));
-  }
-
-  function onModify() {
-    setForcedRedraw((ps) => (ps+1));
-  };
-
-  function onDelete() {
-    setForcedRedraw((ps) => (ps+1));
-  };
 
   useEffect( () => {
 
@@ -60,38 +51,82 @@ function App() {
     <div>
       <NavBar />
 
-        <Switch>
+      <Switch>
 
-          <Route exact path="/quality">
-            <QualityGraph qualityData={qualityData} setQualityData={setQualityData}
+        <Route exact path="/quality">
+          <DataPrep
+            isFromApp={isFromApp} 
+            component={"D3LineGraph"}
+            dataSeriesDemand={"qualityData"} 
+            qualityData={qualityData} 
+            setQualityData={setQualityData}
+            quantityData={quantityData} 
+            setQuantityData={setQuantityData}
             />
-          </Route>
+          {/* <QualityGraph qualityData={qualityData} setQualityData={setQualityData}
+          /> */}
+        </Route>
 
-          <Route exact path="/quantity">
-            <QuantityGraph quantityData={quantityData} setQuantityData={setQuantityData}
-            />
-          </Route>
+        <Route exact path="/quantity">
+          <DataPrep
+            isFromApp={isFromApp} 
+            component={"D3LineGraph"}
+            dataSeriesDemand={"quantityData"} 
+            qualityData={qualityData} 
+            setQualityData={setQualityData}
+            quantityData={quantityData} 
+            setQuantityData={setQuantityData}
+          />
+          {/* <QuantityGraph quantityData={quantityData} setQuantityData={setQuantityData}
+          /> */}
+        </Route>
 
-          <Route exact path="/">
-            <Home />
-          </Route>
+        <Route exact path="/">
+          <DataPrep
+            isFromApp={isFromApp} 
+            component={"Home"}
+            dataSeriesDemand={""} 
+            qualityData={null} 
+            setQualityData={setQualityData}
+            quantityData={null} 
+            setQuantityData={setQuantityData}
+          />
+          {/* <Home /> */}
+        </Route>
 
-        </Switch>
+      </Switch>
 
-        <CreateForm 
-          data={data}
-          dataSeries={dataSeries}
-          onCreate={onCreate}
-          setData={setData}
-        />
-        <DataList 
-          data={data}
-          dataSeries={dataSeries}
-          onDelete={onDelete}
-          onModify={onModify}
-          setData={setData}
-        />
-        
+      <DataPrep
+        isFromApp={isFromApp} 
+        component={"CreateForm"}
+        dataSeriesDemand={null} 
+        qualityData={qualityData} 
+        setQualityData={setQualityData}
+        quantityData={quantityData} 
+        setQuantityData={setQuantityData}
+      />
+      <DataPrep
+        isFromApp={isFromApp} 
+        component={"DataList"}
+        dataSeriesDemand={null} 
+        qualityData={qualityData} 
+        setQualityData={setQualityData}
+        quantityData={quantityData} 
+        setQuantityData={setQuantityData}
+      />
+      {/* <CreateForm 
+        data={data}
+        dataSeries={dataSeries}
+        onCreate={onCreate}
+        setData={setData}
+      />
+      <DataList 
+        data={data}
+        dataSeries={dataSeries}
+        onDelete={onDelete}
+        onModify={onModify}
+        setData={setData}
+      /> */}
     </div>
   );
 }
---
+++ src/components/DataPrep/DataPrep.js
@@ -0,0 +1,99 @@
+
+import React, { useState, useEffect } from 'react';
+// import { Route, Switch } from "react-router-dom";
+// import QualityGraph from "./components/QualityGraph/QualityGraph";
+// import QuantityGraph from "./components/QuantityGraph/QuantityGraph";
+import Home from "../Home/Home";
+import D3LineGraph from "../CreateForm/CreateForm";
+import CreateForm from "../CreateForm/CreateForm";
+import DataList from '../DataList/DataList';
+// import QualityGraph from "../QualityGraph/QualityGraph";
+// import QuantityGraph from "../QuantityGraph/QuantityGraph";
+// import CreateForm from "../CreateForm/CreateForm";
+// import DataList from '../DataList/DataList';
+
+const Api = 
+"https://glorify-the-supreme-god-67d35a.herokuapp.com";
+// "http://localhost:4000";
+
+function DataPrep() {
+
+  const [data, setData] = useState(null)
+  const [forcedRedraw, setForcedRedraw] = useState(0);
+
+  function onCreate() {
+    setForcedRedraw((ps) => (ps+1));
+  }
+
+  function onModify() {
+    setForcedRedraw((ps) => (ps+1));
+  };
+
+  function onDelete() {
+    setForcedRedraw((ps) => (ps+1));
+  };
+
+  // useEffect( () => {
+
+  //   fetch(
+  //     `${Api}/${'qualityData'}`
+  //   )
+  //   .then((r) => r.json())
+  //   .then((da)=>{
+  //     setQualityData(
+  //       da.sort((a,b) => Date.parse(b.ts) - Date.parse(a.ts))
+  //     );
+  //   });
+
+  //   fetch(
+  //     `${Api}/${'quantityData'}`
+  //   )
+  //   .then((r) => r.json())
+  //   .then((da)=>{
+  //     setQuantityData(
+  //       da.sort((a,b) => Date.parse(b.ts) - Date.parse(a.ts))
+  //     );
+  //   });
+  // }, []);
+
+  return   (
+    <div>
+      <NavBar />
+
+        <Switch>
+
+          <Route exact path="/quality">
+            <QualityGraph qualityData={qualityData} setQualityData={setQualityData}
+            />
+          </Route>
+
+          <Route exact path="/quantity">
+            <QuantityGraph quantityData={quantityData} setQuantityData={setQuantityData}
+            />
+          </Route>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+
+        <CreateForm 
+          data={data}
+          dataSeries={dataSeries}
+          onCreate={onCreate}
+          setData={setData}
+        />
+        <DataList 
+          data={data}
+          dataSeries={dataSeries}
+          onDelete={onDelete}
+          onModify={onModify}
+          setData={setData}
+        />
+        
+    </div>
+  );
+}
+
+export default DataPrep;
